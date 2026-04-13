7a47b696f7aeb36da46a26c26082feaceb48334e
John Collins <john_collins@live.com>
2026-04-13 10:53:08 -0700

    added comments outlining structure of program

--- workout-processor.py
+++ workout-processor.py
@@ -1,6 +1,19 @@
 # workout-processor.py
 if __name__ == "__main__":
+    # imports
     import sys
     import pandas as pd
+
+    # take argv[1] filename
+
+    # function to determine file type
+
+    # read() function that takes file according to type starts parsing process
+
+    # check() function to check that data in file is valid workout program and syntax is correct
+
+    # import_data() function that takes the data and fills data into pandas dataframe
+
+    # publish() function that fills data from dataframe into html file
     data = pd.read_excel(sys.argv[1], index_col=0)
     print(data)
