f710e85657ca061dcefbda0f2ba520352f5fa6ec
John Collins <john_collins@live.com>
2026-04-20 20:40:32 -0700

    Started implementing testing with pytest

--- DEVNOTES.md
+++ DEVNOTES.md
@@ -6,9 +6,6 @@
     - line that waits for user input
     - loop iterates as long as user input is not 1 or 2
 
-## Journal
-Ok, so now that I can import the data into a dataframe, I need to fill up a data structure with it.
-
 ## notes
 - the most convenient use for workout processor is running the command
   "workout-processor 4-15-26.xlsx" with an appended workout sheet to convert it into a human
@@ -17,3 +14,10 @@ Ok, so now that I can import the data into a dataframe, I need to fill up a data
 So far, I've had to install the following python libraries:
 - dev-python/openpyxl (pure python reader and writer of Excel OpenXML files)
 - dev-python/pandas (powerful data structures for data analysis and statistics)
+- dev-python/pytest (unit testing framework)
+
+## Functions
+### validate_file(filename)
+- checks if the path exists
+### get_extension(filename)
+- checks if the extension is accepted, then returns it
--- workout-processor.py
+++ workout-processor.py
@@ -1,16 +1,33 @@
 # workout-processor.py
 
 # imports
-import os
 import sys
+from pathlib import Path
 import pandas as pd
+import pdb
+import pytest
 
 # function to determine file type
 # TODO: check that the input provided is valid path, if not throw error
-def get_extension(filename):
-    file_name, file_extension = os.path.splitext(filename)
-    print(file_name)
-    print(file_extension)
+def validate_file(filename):
+    p = Path(filename)
+    if not (p.is_file()):
+        raise FileNotFoundError
+    return True
+
+def get_extension(filename: Path):
+    accepted_extensions = {'.ods', '.pdf', '.csv', '.xlsx'}
+    extension = filename.suffix
+    return extension
+
+# TODO: figure out where to put these tests
+#def test_validate_file():
+#    assert validate_file("Example Workout.xlsx") == True
+#    with pytest.raises(FileNotFoundError):
+#        validate_file("filename.txt")
+#
+#def test_get_extension():
+#    assert get_extension("filename.txt") == '.txt'
 
 # read() function that takes file according to type starts parsing process
 
@@ -20,7 +37,9 @@ def get_extension(filename):
 
 # publish() function that fills data from dataframe into html file
 
+# TODO: If no file is passed as command line argument, print usage message
 if __name__ == "__main__":
-    get_extension(sys.argv[1])
+    validate_file(sys.argv[1])
+    get_extension(Path(sys.argv[1]))
     #data = pd.read_excel(sys.argv[1], index_col=0)
     #print(data)
